5da31b33eee3ad1c3205a3af170961de01d0d865
WESHOK <gdvarta@gmail.com>
2025-01-09 17:21:04 +0300

    solved the first task!

---
+++ src/add_binary.rs
@@ -0,0 +1,12 @@
+struct Solution;
+
+impl Solution {
+    pub fn add_binary(a: String, b: String) -> String {
+        return String::new()
+    }
+}
+
+#[test]
+fn test() {
+    
+}
---
+++ src/find_the_index_of_the_first_occurence_in_a_string.rs
@@ -0,0 +1,27 @@
+struct Solution;
+
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        for (i, _c) in haystack.char_indices() {
+            if haystack.len() - i >= needle.len() {
+                if haystack.get(i..i + needle.len()) == Some(&needle) {
+                    return i.try_into().unwrap()
+                }
+            }
+        }
+        return -1
+    }
+}
+
+#[test]
+fn test() {
+    let haystack = String::from("sadbutsad");
+    let needle = String::from("sad");
+    assert_eq!(Solution::str_str(haystack, needle), 0);
+    let haystack = String::from("leetcode");
+    let needle = String::from("leeto");
+    assert_eq!(Solution::str_str(haystack, needle), -1);
+    let haystack = String::from("hello");
+    let needle = String::from("ll");
+    assert_eq!(Solution::str_str(haystack, needle), 2);
+}
