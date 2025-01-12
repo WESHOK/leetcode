0c356003c3595255b86cee0b3b86edc504052e42
WESHOK <gdvarta@gmail.com>
2025-01-12 13:16:37 +0300

    add_binary solved!

--- src/add_binary.rs
+++ src/add_binary.rs
@@ -1,40 +1,55 @@
 struct Solution;
 
 impl Solution {
-    fn smaller_str(a: String, b: String) -> String {
+    fn align_smaller_str(a: &String, b: &String) -> (String, String) {
+        let mut smaller_str: String;
+        let bigger_str: String;
+
+        let diff = a.len().abs_diff(b.len());
+
         if a.len() > b.len() {
-            a
+            smaller_str = b.clone();
+            bigger_str = a.clone();
         } else {
-            b
+            smaller_str = a.clone();
+            bigger_str = b.clone();
         }
+
+        smaller_str.insert_str(0, &"0".repeat(diff));
+
+        println!("align_str: {}, {}", smaller_str, bigger_str);
+        return (smaller_str, bigger_str);
     }
 
     fn add_binary(a: String, b: String) -> String {
-        let mut inc: u32 = 0;
+        let mut inc = 0u32;
         let mut res = String::new();
 
+        let new_str = Solution::align_smaller_str(&a, &b);
 
+        for (bigger_c, smaller_c) in new_str.1.chars().rev().zip(new_str.0.chars().rev()) {
+            println!("bigger_c: {}, smaller_c: {}", bigger_c, smaller_c);
 
-        for ac in a.chars().rev() {
-            for bc in b.chars().rev() {
-                let ac_digit = ac.to_digit(10).unwrap();
-                let bc_digit = bc.to_digit(10).unwrap();
-                println!("{} {}", ac_digit, bc_digit);
+            let bigger_digit = bigger_c.to_digit(10).unwrap();
+            let smaller_digit = smaller_c.to_digit(10).unwrap();
 
-                let sum = ac_digit + bc_digit;
+            let sum = bigger_digit + smaller_digit + inc;
 
-                if sum == 2 {
-                    inc += 1;
-                }
-
-                let res_digit = (sum + inc) % 2;
-                res.insert_str(0, &res_digit.to_string());
-
-                if inc != 0 {
-                    inc -= 1;
-                }
+            if inc != 0 {
+                inc -= 1;
             }
+
+            let res_digit = sum % 2;
+            
+            res.insert_str(0, &res_digit.to_string());
+
+            inc += sum / 2;
         }
+
+        if inc != 0 {
+            res.insert_str(0, "1");
+        }
+
         return res;
     }
 }
@@ -49,8 +64,4 @@ fn test() {
     let b = String::from("1011");
     let res = String::from("10101");
     assert_eq!(Solution::add_binary(a, b), res);
-    // let a = String::from("11");
-    // let b = String::from("1");
-    // let res = String::from("100");
-    // assert_eq!(Solution::add_binary(a, b), res);
 }
