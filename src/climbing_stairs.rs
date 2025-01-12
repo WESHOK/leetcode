65b65da4ddc2d416c56d34326bbe3defeb4b9c94
WESHOK <gdvarta@gmail.com>
2025-01-12 17:14:25 +0300

    climbing stairs solved

---
+++ src/climbing_stairs.rs
@@ -0,0 +1,28 @@
+struct Solution;
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        if n <= 2 {
+            return n
+        }
+    
+        let mut a = 1;
+        let mut b = 2;
+    
+        for _ in 3..=n {
+            let temp = b;
+            b = a + b;
+            a = temp;
+        }
+    
+        b
+    }
+}
+
+#[test]
+fn test() {
+    let n = 2;
+    assert_eq!(Solution::climb_stairs(n), 2);
+    let n = 3;
+    assert_eq!(Solution::climb_stairs(n), 3);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@
 
 mod find_the_index_of_the_first_occurence_in_a_string;
 mod add_binary;
+mod climbing_stairs;
