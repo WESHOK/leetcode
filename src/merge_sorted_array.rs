bab63f1a91416951a355d0152940b198e4bbe547
WESHOK <gdvarta@gmail.com>
2025-01-13 15:26:32 +0300

    solved

--- src/merge_sorted_array.rs
+++ src/merge_sorted_array.rs
@@ -1,21 +1,37 @@
 struct Solution;
 
 impl Solution {
-    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        for (idx, _i) in nums1.clone().iter().enumerate() {
-            if idx > m as usize - 1 {
-                nums1.remove(idx);
-            }
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, _n: i32) {
+        let temp = nums1[0..m as usize].to_vec();
+        for i in &temp {
+            print!("{} ", i);
         }
-        for (idx, _i) in nums2.clone().iter().enumerate() {
-            if idx > n as usize - 1 {
-                nums1.remove(idx);
-            }
-        }
+        nums1.clear();
+        nums1.extend(temp);
+        nums1.extend(nums2.to_vec());
+
+        nums1.sort();
     }
 }
 
 #[test]
 fn test() {
-
+    let mut nums1 = vec![1, 2, 3, 0, 0, 0];
+    let m = 3;
+    let mut nums2 = vec![2, 5, 6];
+    let n = 3;
+    Solution::merge(&mut nums1, m, &mut nums2, n);
+    assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
+    let mut nums1 = vec![1];
+    let m = 1;
+    let mut nums2 = vec![];
+    let n = 0;
+    Solution::merge(&mut nums1, m, &mut nums2, n);
+    assert_eq!(nums1, vec![1]);
+    let mut nums1 = vec![0];
+    let m = 0;
+    let mut nums2 = vec![1];
+    let n = 1;
+    Solution::merge(&mut nums1, m, &mut nums2, n);
+    assert_eq!(nums1, vec![1]);
 }
